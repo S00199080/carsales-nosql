11d4bca25abc68190f6d467b523781302b14c11b
Jonathan Grehan-Butler <S00199080@mail.itsligo.ie>
2021-03-31 19:25:08 +0100

    Fixed a small issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,11 @@ export const environment = {
   production: true,
 
   firebase:{
-  apiKey: "AIzaSyDAl5VXxoW4ZlPtSJ3XcOIoFuKlRn1Jx6s",
-    authDomain: "carsales-d0f1c.firebaseapp.com",
-    projectId: "carsales-d0f1c",
-    storageBucket: "carsales-d0f1c.appspot.com",
-    messagingSenderId: "373011420955",
-    appId: "1:373011420955:web:9abc1493a4b92222abe5be",
-    measurementId: "G-NW43CK7ELZ"}
+    apiKey: "AIzaSyAABfrbmjmeTNfn-gxl2XGRpwc6cCChYGs",
+    authDomain: "carsales-7cbf0.firebaseapp.com",
+    projectId: "carsales-7cbf0",
+    storageBucket: "carsales-7cbf0.appspot.com",
+    messagingSenderId: "100332278302",
+    appId: "1:100332278302:web:ad4133630a06d15985d40f",
+    measurementId: "G-1YTK1C5WDP"}
 };
